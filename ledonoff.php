78d2e15a13a965b124a057a68c77d192861445b1
Kurt <akurt@campus.unibe.ch>
2019-07-10 11:36:50 +0200

    -

--- ledonoff.php
+++ ledonoff.php
@@ -1,14 +1,14 @@
 <?php
 
 $onoroff = $_GET['state'];
-$textfile = "/mysensor/ledstate.txt";
+$textfile = "../mysensor/ledstate.txt";
 
 $file = fopen($textfile, 'w') or die("Something went wrong!");
 $stringToWrite = "$onoroff"; // Write either 1 or 0 depending on request from index.html
 fwrite($file, $stringToWrite); // Writes it to the .txt file
 fclose($file);
 
-header('Location: mysensor.php'); // Return to frontend
+header('Location: index.php'); // Return to frontend
 
 ?>
 
--- ledonoff1.php
+++ ledonoff1.php
@@ -1,7 +1,7 @@
 <?php
 
 $onoroff = $_GET['state'];
-$textfile = "/mysensor/ledstate.txt";
+$textfile = "../mysensor/ledstate.txt";
 
 $file = fopen($textfile, 'w') or die("Something went wrong!");
 $stringToWrite = "$onoroff"; // Write either 1 or 0 depending on request from index.html
