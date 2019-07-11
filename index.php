d7d2e1c09139bba7b3570eca9b9641eb7c269947
Kurt <akurt@campus.unibe.ch>
2019-07-11 16:04:29 +0200

    -

--- index.php
+++ index.php
@@ -113,7 +113,7 @@ include('dbconnect.php');
                 <div class="row">
                     <div class="col-sm-3">
                         <div class="card" style="width: 18rem;">
-                            <img class="card-img-top" src="sprite_new1.png" id="hum" alt="Card image cap">
+                            <img class="card-img-top" id="hum" src="sprite_new1.png" alt="Card image cap">
                             <div class="card-body">
                                 <h5 class="card-title">Luftfeuchtigkeit</h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
