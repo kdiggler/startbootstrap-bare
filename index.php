cf9a697662461822e54035716d879122549378b1
Kurt <akurt@campus.unibe.ch>
2019-07-12 14:22:44 +0200

    -

--- index.php
+++ index.php
@@ -138,7 +138,7 @@ include('dbconnect.php');
                             <div class="card-header">
                                 Aussentemperatur
                             </div>
-                            <img class="card-img-top" src="/images/pictogram/temp1.png" alt="Card image cap">
+                            <img class="card-img-top" src="images/pictogram/temp1.png" alt="Card image cap">
                             <div class="card-body">
                                 <p>
                                     <strong><?= $row2['celsius'] ?> &deg;C</strong>
@@ -155,7 +155,7 @@ include('dbconnect.php');
                             <div class="card-header">
                                 Innentemperatur
                             </div>
-                            <img class="card-img-top" src="/images/pictogram/temp1.png" alt="Card image cap">
+                            <img class="card-img-top" src="images/pictogram/temp1.png" alt="Card image cap">
                             <div class="card-body">
                                 <h5 class="card-title">Special title treatment</h5>
                                 <p>
