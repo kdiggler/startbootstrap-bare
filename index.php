93b7846ef9b2eaee6c70f871cd15c05dc6104787
Kurt <akurt@campus.unibe.ch>
2019-07-18 16:49:21 +0200

    -

--- index.php
+++ index.php
@@ -179,6 +179,10 @@ include('dbconnect.php');
         </div>
     </div>
 
+    <footer class="container-fluid text-center">
+  <p>Footer Text</p>
+</footer>
+
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.slim.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
