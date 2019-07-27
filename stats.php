6e111d70d14cee626e5eabc9bdd2b998396cac9a
Kurt <akurt@campus.unibe.ch>
2019-07-28 01:54:01 +0200

    -

--- stats.php
+++ stats.php
@@ -164,82 +164,9 @@ include('functions.php');
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                    <div class="col-sm-4">
-                        <div class="card">
-                            <div class="card-header">
-                                Luftfeuchtigkeit
-                            </div>
-                            <div class="card-body">
-                                <p>TOP 5</p>
-                                <p style="color:black;font-size:22px;"></p>
-                                <table class="table table-striped w-100">
-                                    <thead>
-                                        <tr>
-                                            <th>Datum</th>
-                                            <th>Hum %</th>
-                                        </tr>
-                                    </thead>
-
-                                    <tbody>
-                                        <?= relhum(); ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-4">
-                        <div class="card">
-                            <div class="card-header">
-                                Innentemperatur
-                            </div>
-                            <div class="card-body">
-                                <p>TOP 5</p>
-                                <p style="color:black;font-size:22px;"></p>
-
-                                <table class="table table-striped w-100">
-                                    <thead>
-                                        <tr>
-                                            <th>Datum</th>
-                                            <th>Temp °C</th>
-                                        </tr>
-                                    </thead>
-
-                                    <tbody>
-                                        <?= tempindoor(); ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-4">
-                        <div class="card">
-                            <div class="card-header">
-                                Wassertemperatur
-                            </div>
-                            <div class="card-body">
-                                <p>TOP 5</p>
-                                <p style="color:black;font-size:22px;"></p>
-
-                                <table class="table table-striped w-100">
-                                    <thead>
-                                        <tr>
-                                            <th>Datum</th>
-                                            <th>Temp °C</th>
-                                        </tr>
-                                    </thead>
-
-                                    <tbody>
-                                        <?= tempaqua(); ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
-    </div>
     </div>
     <div class="footer">D'Site isch vor Gruppe BI designt u programmiert , <strong>no cöpyreit 2019</strong>.</div>
 
