71f7873d14572f7dc1227f28d1464386a320691a
Kurt <akurt@campus.unibe.ch>
2019-07-28 01:57:46 +0200

    -

--- functions.php
+++ functions.php
@@ -93,4 +93,27 @@ function tempindoor_min()
     mysqli_close($con);
 }
 
+#TEMPERATUR WASSER FUNCTION MIN
+function tempaqua_min()
+{
+    $con = mysqli_connect("localhost", "arduser", "ardtest", "arduino");
+
+    /* check connection */
+    if (mysqli_connect_errno()) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    }
+    $query = "SELECT MIN(celsius) AS celsius, event, DATE(event) AS date_created FROM aquatemp GROUP BY DATE(event) ORDER BY celsius desc LIMIT 5";
+   # $query = "SELECT ROUND(celsius, 1) AS celsius, event FROM aquatemp ORDER BY celsius desc LIMIT 5";
+
+    if ($result = mysqli_query($con, $query)) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo"<tr><td>{$row["event"]}</td><td>{$row["celsius"]}</td></tr>\n";
+        }
+    }
+
+    /* close connection */
+    mysqli_close($con);
+}
+
 ?>
