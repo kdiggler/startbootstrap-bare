898023cb5d2439fda7748e779a2c67b101ea5e15
Kurt <akurt@campus.unibe.ch>
2019-08-03 11:40:25 +0200

    -

--- stats.php
+++ stats.php
@@ -207,7 +207,7 @@ include('functions.php');
                             </div>
                         </div>
                     </div>
-                    <p>
+                    <br>
 
                     <div class="col-sm-4">
                         <div class="card">
