1aaa584dc075101f7d16b7918440584fd2b5eeb2
Kurt <akurt@campus.unibe.ch>
2020-10-26 22:02:15 +0100

    -

--- dbtest.php
+++ dbtest.php
@@ -9,9 +9,11 @@ include('dbconnect.php');
 
     $SQL = "INSERT INTO arduino.outdoortemp1 (sensor, celsius) VALUES ('WS_temp1', '$outdoortemp_ext')";
 
-    <strong><?= $outdoortemp_ext['celsius'] ?> &deg;C</strong>
-
-    // Execute SQL statement
+// Execute SQL statement
     mysqli_query($con,$SQL);
+    
 
 ?>
+
+<strong><?= $outdoortemp_ext['celsius'] ?> &deg;C</strong>
+
