8b318e652f5e48c6f5c7e798e78dc7ba88cf3bf5
Kurt <akurt@campus.unibe.ch>
2020-10-26 22:27:47 +0100

    -

--- dbtest.php
+++ dbtest.php
@@ -3,7 +3,7 @@
 include('dbconnect_external.php');
 include('dbconnect.php');
 
-    $query = "SELECT ROUND(celsius, 1) AS celsius FROM outdoortemp1 ORDER BY id desc LIMIT 1";
+    $query = "SELECT ROUND(celsius, 1) AS celsius FROM outdoortemp1 ORDER BY id desc";
     $result = mysqli_query($con_ext, $query);
 #    $outdoortemp_ext =  mysqli_fetch_assoc($result);
 
