3261d258dbf87433f0226d2faec469145873e8b8
Kurt <akurt@campus.unibe.ch>
2020-10-26 21:45:46 +0100

    -

--- dbconnect_external.php
+++ dbconnect_external.php
@@ -1,7 +1,7 @@
 <?php
-$con = mysqli_connect('kdiggler.ddns.net:3306', 'aquacontrol', 'aqua$1234', 'aquacontroldb');
-if($con->connect_errno > 0){
-    die('Unable to connect to database [' . $con->connect_error . ']');
+$con_ext = mysqli_connect('kdiggler.ddns.net:3306', 'aquacontrol', 'aqua$1234', 'aquacontroldb');
+if($con_ext->connect_errno > 0){
+    die('Unable to connect to database [' . $con_ext->connect_error . ']');
 }
 // $dbh = mysql_connect($MyHostname , $MyUsername, $MyPassword);
 //$selected = mysqli_select_db("arduino",$conn);
--- dbtest.php
+++ dbtest.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <?php
 include('dbconnect_external.php');
-?>
 
-<?php
     $query = "SELECT ROUND(celsius, 1) AS celsius FROM outdoortemp1 ORDER BY id desc";
-    $result = mysqli_query($con, $query);
-    $row2 =  mysqli_fetch_assoc($result);
+    $result = mysqli_query($con_ext, $query);
+    $outdoortemp_ext =  mysqli_fetch_assoc($result);
 
-?>    
+    $SQL = "INSERT INTO arduino.outdoortemp1 (sensor, celsius) VALUES ('WS_temp1', '$outdoortemp_ext')";
 
-<strong><?= $row2['celsius'] ?> &deg;C</strong>
+?>
