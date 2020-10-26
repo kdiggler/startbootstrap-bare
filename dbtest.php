007290bfd22f66a7cb973e1c175b3ed4a793168f
Kurt <akurt@campus.unibe.ch>
2020-10-26 22:28:37 +0100

    -

--- dbtest.php
+++ dbtest.php
@@ -5,9 +5,9 @@ include('dbconnect.php');
 
     $query = "SELECT ROUND(celsius, 1) AS celsius FROM outdoortemp1 ORDER BY id desc";
     $result = mysqli_query($con_ext, $query);
-#    $outdoortemp_ext =  mysqli_fetch_assoc($result);
+    $outdoortemp_ext =  mysqli_fetch_assoc($result);
 
-    $SQL = "INSERT INTO arduino.outdoortemp1 (sensor, celsius) VALUES ('WS_temp1', '$result[celsius]')";
+    $SQL = "INSERT INTO arduino.outdoortemp1 (sensor, celsius) VALUES ('WS_temp1', '$outdoortemp_ext[celsius]')";
 
 // Execute SQL statement
     mysqli_query($con,$SQL);
